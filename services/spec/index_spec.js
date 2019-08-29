b8eaaee6e120d5eaa0d10c7b3cc288526d28b97e
Kimura Kosuke <cwkkimura@kimuranoMacBook-Pro.local>
2019-08-29 14:28:00 +0900

    study lambda

--- services/spec/index_spec.js
+++ services/spec/index_spec.js
@@ -3,7 +3,8 @@ describe('lambda function', function() {
   var context;
 
   beforeEach(function() {
-    context = jasmine.createSpyObj('context', ['succeed']);
+    context = jasmine.createSpyObj('context', ['succeed', 'fail']);
+    index.dynamodb = jasmine.createSpyObj('dynamo', ['scan'])
   });
 
   describe('echo', function() {
@@ -13,4 +14,26 @@ describe('lambda function', function() {
       expect(context.succeed).toHaveBeenCalledWith(expected);
     });
   });
-});
+
+  describe('popularAnswers',function(){
+    it('requests problems with given problem number', function(){
+      index.popularAnswers({problemNumber: 42}, context)
+      expect(index.dynamodb.scan).toHaveBeenCalledWith({
+        FilterExpression: "problemId = :problemId",
+        ExpressionAttributeValues: {":problemId": 42},
+        TableName: 'learnjs'
+      }, jasmine.any(Function))
+    })
+    it('groups answers by minified code', function(){
+      index.popularAnswers({problemNumber: 1}, context);
+      index.dynamodb.scan.calls.first().args[1](undefined, {Items: [
+        { answer: "true" },
+        { answer: "true" },
+        { answer: "true" },
+        { answer: "!false" },
+        { answer: "!false" },
+      ]});
+      expect(context.succeed).toHaveBeenCalledWith({"true": 3, "!false": 2})
+    })
+  })
+})
